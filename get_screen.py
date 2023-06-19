dfad2041f64a75c169b7a36b40d61c1a9c8807be
Watchdog0x <84445039+Watchdog0x@users.noreply.github.com>
2023-06-19 21:10:45 +0200

    bug fix get_screen.py

--- get_screen.py
+++ get_screen.py
@@ -1,7 +1,6 @@
 import ctypes
 from ctypes import wintypes
 
-# Define the MONITORINFOEX structure
 class MONITORINFOEX(ctypes.Structure):
     _fields_ = [
         ('cbSize', wintypes.DWORD),
@@ -11,81 +10,66 @@ class MONITORINFOEX(ctypes.Structure):
         ('szDevice', wintypes.WCHAR * 32),
     ]
 
-class GetScreen:    
-    def __init__(self, screen):
-        self.monitor_handles = self.get_display_monitors()
-        self.monitor_info_dict = {}
+class GetScreen:
+    def __init__(self, screen: int):
+        self.__monitor_handles = self.__get_display_monitors()
+        self.__monitor_info_dict = {}
 
-        if screen < len(self.monitor_handles):
-            monitor_info = self.get_monitor_info(screen)
-            self.monitor_info_dict[screen] = monitor_info
-            self.monitor = monitor_info.rcMonitor
-            self.display_name = self.get_device_name(screen)
+        if screen < len(self.__monitor_handles):
+            monitor_info = self.__get_monitor_info(screen)
+            self.__monitor_info_dict[screen] = monitor_info
+            self.__monitor = monitor_info.rcMonitor
+            self.display_name = self.__get_device_name(screen)
+            self.is_primary_screen = self.__is_primary_screen(screen)
         else:
             raise ValueError("Invalid screen number")
 
-    def get_monitor_info(self, screen):
+    def __get_monitor_info(self, screen: int) -> MONITORINFOEX:
         monitor_info = MONITORINFOEX()
-        monitor_info.cbSize = ctypes.sizeof(MONITORINFOEX)
-        ctypes.windll.user32.GetMonitorInfoW(self.monitor_handles[screen], ctypes.byref(monitor_info))
+        monitor_info.cbSize = ctypes.sizeof(monitor_info)
+        ctypes.windll.user32.GetMonitorInfoW(self.__monitor_handles[screen], ctypes.byref(monitor_info))
         return monitor_info
 
-    def get_monitor(self, screen):
-        monitor_info = self.monitor_info_dict.get(screen)
-        if monitor_info:
-            return monitor_info.rcMonitor
-        else:
-            raise ValueError("Invalid screen number")
-
-    def get_device_name(self, screen):
-        monitor_info = self.monitor_info_dict.get(screen)
+    def __get_device_name(self, screen: int) -> str:
+        monitor_info = self.__monitor_info_dict.get(screen)
         if monitor_info:
             device_name = monitor_info.szDevice.replace('\\\\.\\', '')
             return device_name
         else:
             raise ValueError("Invalid screen number")
 
-    def get_display_monitors(self):
-        class RECT(ctypes.Structure):
-            _fields_ = [
-                ('left', ctypes.c_long),
-                ('top', ctypes.c_long),
-                ('right', ctypes.c_long),
-                ('bottom', ctypes.c_long),
-            ]
+    def __get_display_monitors(self) -> list:
+        monitors = []
 
         EnumDisplayMonitorsProc = ctypes.WINFUNCTYPE(
             ctypes.c_int,
             ctypes.c_ulong,
             ctypes.c_ulong,
-            ctypes.POINTER(RECT),
+            ctypes.POINTER(wintypes.RECT),
             ctypes.c_double,
         )
 
-        def callback(hMonitor, hdcMonitor, lprcMonitor, dwData):
+        def callback(hMonitor: ctypes.c_ulong, hdcMonitor: ctypes.c_ulong, lprcMonitor: ctypes.POINTER(wintypes.RECT),
+                     dwData: ctypes.c_double) -> bool:
             monitors.append(hMonitor)
             return True
 
-        monitors = []
         enum_proc = EnumDisplayMonitorsProc(callback)
         ctypes.windll.user32.EnumDisplayMonitors(None, None, enum_proc, 0)
         return monitors
 
-    def left_top_right_bottom(self):
-        return self.monitor.left, self.monitor.top, self.monitor.right, self.monitor.bottom
+    @property
+    def left_top_right_bottom(self) -> tuple:
+        return self.__monitor.left, self.__monitor.top, self.__monitor.right, self.__monitor.bottom
 
-    def x_y_width_height(self):
-        return self.monitor.left, self.monitor.top, self.monitor.right - self.monitor.left, self.monitor.bottom - self.monitor.top
+    @property
+    def x_y_width_height(self) -> tuple:
+        return self.__monitor.left, self.__monitor.top, self.__monitor.right - self.__monitor.left, self.__monitor.bottom - self.__monitor.top
 
-    def is_primary_screen(self):
-        monitor_info = self.monitor_info_dict.get(screen)
+    def __is_primary_screen(self, screen: int) -> bool:
+        monitor_info = self.__monitor_info_dict.get(screen)
         if monitor_info:
-            return monitor_info.dwFlags & 1  # MONITORINFOF_PRIMARY
+            return monitor_info.dwFlags & 1 == 1  # MONITORINFOF_PRIMARY
         else:
             raise ValueError("Invalid screen number")
-
-    # Alias
-    ltrb = left_top_right_bottom
-    xywh = x_y_width_height
-
 
